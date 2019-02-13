b188822ad00051f72dadef57308a35777f373e87
Ajay-Singhroha <email.ajaysinghroha@gmail.com>
2019-02-13 12:20:22 +0530

    Changed logo of cynosure, made the navbar responsive, did some slignments and small changes

--- script.js
+++ script.js
@@ -206,3 +206,24 @@ function fauxClick(x, y) {
   fauxClick.pageY = y;
   document.dispatchEvent(fauxClick);
 }
+
+
+//Progress bar function
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//responsive navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
